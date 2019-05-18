1d6db783c82c1f24fe08f063ffc6606b69107e17
jko <junheee.ko@gmail.com>
2019-05-18 13:37:32 +0900

    ADD : Product Controller/Entity/Repository

--- src/main/java/com/shopping/jpa/repository/ProductRepository.java
+++ src/main/java/com/shopping/jpa/repository/ProductRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface CategoryRepository extends JpaRepository<Product, Long> {
+interface ProductRepository extends JpaRepository<Product, Long> {
 }
